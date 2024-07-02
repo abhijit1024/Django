ac33af27523ac0f0a106f7b9bcecffeb624501be
abhijit1024 <abhi.decoder1@gmail.com>
2024-07-02 21:11:19 +0530

    Added new feature in tasks with session

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,32 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
-tasks=["foo","bar","baz"]
+class NewTaskform(forms.Form):
+    task = forms.CharField(label =" New task ")
+##  priority =forms.IntegerField(label="priority",min_value=1,max_value=10)
+
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
     return render(request,"tasks/index.html",{
-        "tasks":tasks
+        "tasks":request.session["tasks"]
     })
 
 def add(request):
-    return render(request,"tasks/add.html")
+    if request.method == "POST":
+        form=NewTaskform(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            request.session["tasks"]+=[task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request,"tasks/add.html",{
+                "form":form
+            })
+    return render(request,"tasks/add.html",{
+        "form" :NewTaskform()
+    })
